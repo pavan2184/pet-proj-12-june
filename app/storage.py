8da1d758f76fb5cfeae2daeba9df9772960f225e
Pavan <pavanmadhup@gmail.com>
2025-06-12 17:12:45 +0800

    Added test values in pet storage

--- app/storage.py
+++ app/storage.py
@@ -1,1 +1,20 @@
-pets = {}
+from app.models import Pet
+from datetime import datetime
+from uuid import uuid4
+
+pets = {
+    str(uuid4()): Pet(
+        id=str(uuid4()),
+        name="Buddy",
+        type="Dog",
+        age=3,
+        created_at=datetime.utcnow()
+    ),
+    str(uuid4()): Pet(
+        id=str(uuid4()),
+        name="Mittens",
+        type="Cat",
+        age=2,
+        created_at=datetime.utcnow()
+    )
+}
